485a544bc9c1f91a2120b2bd201acbb324d24ea8
Mike Okovityy <35541518+MikeOkovityy@users.noreply.github.com>
2022-08-04 20:31:34 +0300

    setup weather labels

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -8,25 +8,45 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
     
     var currentWeather: CurrentWeather?
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperaturaLabel: UILabel!
+    @IBOutlet weak var weatherDiscription: UILabel!
+    @IBOutlet weak var minMaxTemperaturaLabel: UILabel!
+    
+    
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var visabilityLabel: UILabel!
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(currentWeather?.name)
-        // Do any additional setup after loading the view.
+        setupLabels()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupLabels() {
+        guard let currentWeather = currentWeather else { return }
+    
+        cityNameLabel.text = currentWeather.name
+        temperaturaLabel.text = "\(currentWeather.main.temp)°"
+        weatherDiscription.text = currentWeather.weather[0].description
+        minMaxTemperaturaLabel.text = "min   \(currentWeather.main.temp_min)° max   \(currentWeather.main.temp_max)°"
+        
+        
+        humidityLabel.text = "\(currentWeather.main.humidity)%"
+        pressureLabel.text = "\(currentWeather.main.pressure)mPa"
+        visabilityLabel.text = "\(currentWeather.visibility)m"
+        feelsLikeLabel.text = "\(currentWeather.main.feels_like)"
     }
-    */
+    
 
 }
